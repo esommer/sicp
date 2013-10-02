431832052d7d2192ccbef0f84e163b41d1a5d6d9
Emily Sommer <emily.sommer@gmail.com>
2013-10-02 19:25:45 -0400

    saved racket file

--- ex1-1-3.rkt
+++ ex1-1-3.rkt
@@ -1,1 +1,7 @@
 #lang planet neil/sicp
+(define (lss x y z)
+    (cond ((and (< x y) (< x z)) (+ (* y y) (* z z)))
+          ((and (< y x) (< y z)) (+ (* x x) (* z z)))
+          (else (+ (* x x) (* y y)))
+       )
+    )
