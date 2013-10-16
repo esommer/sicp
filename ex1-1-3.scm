b011fec129dbcfc0a017e6f86a2c18f3cc31d918
esommer <emily.sommer@gmail.com>
2013-10-16 12:25:23 -0400

    added exercise 1.3

--- ex1-1-3.scm
+++ ex1-1-3.scm
@@ -1,7 +1,8 @@
-# PROBLEM: Define a procedure that takes 3 numbers as arguments and returns the sum of the squares of the 2 larger numbers.
+; PROBLEM: Define a procedure that takes 3 numbers as arguments and returns the sum of the squares of the 2 larger numbers.
 (define (lss x y z)
     (cond ((and (< x y) (< x z)) (+ (* y y) (* z z)))
           ((and (< y x) (< y z)) (+ (* x x) (* z z)))
           (else (+ (* x x) (* y y)))
        )
     )
+(lss 2 3 4)
