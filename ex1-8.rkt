398a170a80afa18098b9535c118f545ab61d9d20
esommer <emily.sommer@gmail.com>
2013-10-16 13:45:43 -0400

    changed name of file

---
+++ ex1-8.rkt
@@ -0,0 +1,25 @@
+#lang scheme
+;exploring cubed roots
+(define (square y) (* y y))
+
+(define (improve guess x)
+  (/ (+ (/ x (square guess)) (* 2 guess)) 3))
+
+(define (abs x)
+  (if (< x 0)
+      (- x)
+      x))
+
+(define (good-enough? guess x)
+  (< (abs (- (square guess) x)) 0.01))
+
+(define (cubert-iter guess x)
+  (if (good-enough? guess x)
+          guess
+          (cubert-iter (improve guess x)
+                     x)))
+
+(define (cubert x)
+  (cubert-iter 1.0 x))
+
+(cubert 27)
